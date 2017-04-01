f42aa76ebe3ff3a823bb95a50de50a7271aaa553
agcedesarrollo <agce.desarrollo@gmail.com>
2017-04-01 07:26:17 +0000

    validaciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-   has_many :suscriptions
+    
+    has_many :suscriptions
+    
+    before_save { self.email = email.downcase }
+    validates :username, presence: true
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 105}, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+    validates :direccion, presence: true
+    validates :colonia, presence: true
+    validates :ciudad, presence: true
+    validates :estado, presence: true
+    validates :cp, presence: true
+    validates :telefono, presence: true, length: {minimum: 7, maximum: 20}
+    
 end
